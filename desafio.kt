116fa2ac4a708011556a79338a4f01b2fb321dbf
guifxb <107308140+guifxb@users.noreply.github.com>
2022-12-13 14:34:58 -0300

    Atualização do programa de matrículas

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,48 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO}
 
-class Usuario
+data class Usuario (val nome: String, var cursando: String = "nao matriculado") { //Atualizado para data class e inclusas as propriedades nome, curso que está cursando e conteúdo atual
+    var conteudoAtual: List<ConteudoEducacional> = listOf()
+    }
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class ConteudoEducacional(val nome: String, val duracao: Int, val nivel: Nivel)  // Cada conteúdo pode ter um nível e duração personalizada
+
+data class Formacao(val nome: String, val conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
-    
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+
+    fun matricular(vararg usuario: Usuario) {
+        usuario.forEach {
+            inscritos.add(it)
+            it.cursando = nome
+            it.conteudoAtual = conteudos.filter { it.nivel == Nivel.BASICO}  // matricula a turma ou um usuário no curso e atribui todos os conteúdos básicos
+        }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+
+    val kotlin101 = ConteudoEducacional("Kotlin101", 60, Nivel.BASICO)
+    val intro = ConteudoEducacional("Introdução à programação", 60, Nivel.BASICO)
+    val kotlin102 = ConteudoEducacional("Kotlin102", 60, Nivel.INTERMEDIARIO)
+    val kotlin201 = ConteudoEducacional("Kotlin201", 60, Nivel.AVANCADO)   // conteúdos para teste
+
+    val formacaoKotlin = Formacao("Formacao Kotlin", listOf(kotlin101, kotlin102, kotlin201, intro)) //formação para teste
+    val turma: MutableList<Usuario> = mutableListOf(Usuario("Jonas"),                         // turma para teste
+        Usuario("Lucas"),
+        Usuario("Tia"),
+        Usuario("Getulio"),
+        Usuario("Iris"),
+        Usuario("Marcos"))
+
+
+    turma.forEach {
+        formacaoKotlin.matricular(it)
+        println("O usuário ${it.nome} acaba de se matricular no curso ${formacaoKotlin.nome}") // matriculuando a turma toda em batch
+    }
+    turma.forEach {
+        println("O usuário ${it.nome} está matriculado em ${it.cursando}. Atualmente, assiste à: ${it.conteudoAtual.map{ it.nome }.joinToString()}") // conferindo se cada usuário foi matriculado no curso e conteúdos corretamente
+    }
 }
